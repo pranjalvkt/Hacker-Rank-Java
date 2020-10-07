a804203c3b0aa41bed1ddeef5d938ef84d41d14e
Pranjal Tripathi <64363696+pranjalvkt@users.noreply.github.com>
2020-10-07 19:12:30 +0530

    update

--- Stack.java
+++ Stack.java
@@ -3,10 +3,10 @@ class Solution{
 	
 	public static void main(String []argh)
 	{
-		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		
-		while (sc.hasNext()) {
-			String input=sc.next();
+		while (scanner.hasNext()) {
+			String input=scanner.next();
             while(input.length() != (input = input.replaceAll("\\(\\)|\\[\\]|\\{\\}", "")).length());
             System.out.println(input.isEmpty());
         }
